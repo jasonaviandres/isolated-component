acc1094e7d7197a62916aaa19106fa03a2e3a177
Jason Aviandre <jasonaviandre@gmail.com>
2023-09-03 14:07:22 +0800

    ✨feature: add button component

---
+++ src/stories/components/Button/Button.stories.tsx
@@ -0,0 +1,56 @@
+import { Button } from "./Button";
+import { Meta, StoryObj } from "@storybook/react";
+import { FaLock } from "react-icons/fa";
+
+const meta = {
+    component: Button,
+    title: "Button",
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const PrimaryButton = {
+    args: {
+        children: "Connect",
+        iconLeft: <FaLock size={12} />,
+    },
+} satisfies Story;
+
+export const SecondaryButton = {
+    args: {
+        children: "Login",
+        variant: "secondary",
+        size: "medium",
+    },
+} satisfies Story;
+
+export const LoadingButton = {
+    args: {
+        loading: true,
+        children: "Please wait...",
+        size: "medium",
+    },
+} satisfies Story;
+
+export const DisbaledButton = {
+    args: {
+        disabled: true,
+        children: "Disabled",
+    },
+} satisfies Story;
+
+export const OutlinedPrimaryButton = {
+    args: {
+        variant: "outlined-primary",
+        children: "Login",
+    },
+} satisfies Story;
+
+export const DisabledOutlinedPrimaryButton = {
+    args: {
+        variant: "outlined-primary",
+        disabled: true,
+        children: "Disabled",
+    },
+};
